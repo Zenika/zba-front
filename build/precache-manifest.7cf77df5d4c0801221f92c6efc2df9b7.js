6f80750ccab5a8460f2275aa7a52a2dbe3479495
Henri <henribom@hotmail.fr>
2019-03-05 19:12:30 +0100

    First commit

---
+++ build/precache-manifest.7cf77df5d4c0801221f92c6efc2df9b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ba682efc73a00e81575b03553a4d2a7",
+    "url": "/static/media/zenika.7ba682ef.svg"
+  },
+  {
+    "revision": "e7d6e90ef927df4672c0d74c840532bc",
+    "url": "/static/media/zba.e7d6e90e.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9ca93aaf2737e54d2bb2",
+    "url": "/static/js/main.9ca93aaf.chunk.js"
+  },
+  {
+    "revision": "29ddfd42cfc27480b293",
+    "url": "/static/js/2.29ddfd42.chunk.js"
+  },
+  {
+    "revision": "9ca93aaf2737e54d2bb2",
+    "url": "/static/css/main.c9fa4c3a.chunk.css"
+  },
+  {
+    "revision": "8ca6fef868c77c3e271a82d3dd3f6218",
+    "url": "/index.html"
+  }
+];
---
+++ src/RecipeTable.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import "./App.css"
+
+class IsRecipe extends Component {
+  render() {
+    if(this.props.recipe.length === 0) {
+      return(<h2>there is no recipe :'(</h2>)
+    } else {
+      console.log(this.props.recipe)
+      return (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Recipe</th>
+              <th>Type of beer</th>
+              <th>Type of malt</th>
+              <th>Creator</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.recipe.map((recipe) => (
+              <tr className="line" key={recipe.name}>
+                <td>{recipe.name}</td>
+                <td>{recipe.ingredientType}</td>
+                <td>{recipe.malt}</td>
+                <td>{recipe.creator}</td>
+                <td>
+                  <button className="button smallButton">Edit</button>
+                  <button className="button smallButton" onClick={() => this.props.delete(recipe.name)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )
+    }
+  }
+}
+
+class RecipeTable extends Component {
+  state = {
+    recipe: []
+  }
+
+  /*Affiche la table des recettes au démarrage de la page*/
+  componentDidMount() {
+    axios.get(`http://localhost:8080/Recipe`)
+      .then(res => {
+        const recipe = res.data;
+        this.setState({ recipe });
+    })
+  }
+
+
+  /*Actualise la table des recettes quand un composant est créer*/
+  componentDidUpdate(prevProps) {
+    if (this.props.update !== prevProps.update) {
+      axios.get(`http://localhost:8080/Recipe`)
+        .then(res => {
+          const recipe = res.data;
+          this.setState({ recipe });
+      })
+    }
+  }
+
+  Delete(i) {
+    axios.delete('http://localhost:8080/Recipe'+i)
+      .then((result) => {
+        console.log("Succesfully deleted")
+    });
+    this.props.setUpdate()
+  }
+
+  render() {
+    return(
+      <div className="column2">
+        <div className="inside">
+          <h1>List recipe</h1>
+          <IsRecipe recipe={this.state.recipe} delete={this.Delete}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default RecipeTable;
